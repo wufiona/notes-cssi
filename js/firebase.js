6c6e5ac31049345e918bbcf63e8f9d25a1003012
Glitch (cssi-day12-notes) <none>
2021-07-29 22:16:29 +0000

    ☂️🎀 Updated with Glitch

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBMTg1LEkMX6q4OlCRS_gKGPcWLrQUX6r0",
+  authDomain: "cssi-day12-10d1a.firebaseapp.com",
+  databaseURL: "https://cssi-day12-10d1a-default-rtdb.firebaseio.com",
+  projectId: "cssi-day12-10d1a",
+  storageBucket: "cssi-day12-10d1a.appspot.com",
+  messagingSenderId: "665542975519",
+  appId: "1:665542975519:web:1c49a72271775b3934352a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
